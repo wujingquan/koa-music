76441887b6498c4e095908c24b477d0770954844
wujingquan <i@wujingquan.com>
2018-10-28 14:56:28 +0800

    add lyric api

--- app.js
+++ app.js
@@ -59,6 +59,23 @@ router.get('/api/getCdInfo', async (ctx, next) => {
   ctx.body = ret;
 });
 
+router.get('/api/lyric', async (ctx, next) => {
+  const url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg';
+  const ret = await axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: ctx.query
+  }).then((res) => {
+    return res.data;
+  }).catch((err) => {
+    console.log(err);
+  })
+
+  ctx.body = ret;
+});
+
 // // 加载中间件
 app.use(bodyParser());
 debug && app.use(cors());
